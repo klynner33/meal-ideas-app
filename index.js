f2585029ce13caaf61a22644141288179a838ed4
klynner33 <113389068+klynner33@users.noreply.github.com>
2023-11-28 11:04:32 -0600

    Got meal name, category, and image from the api and showing in the dom

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
-  // const url = "https://www.themealdb.com/api/json/v1/1/random.php"
+  const url = "https://www.themealdb.com/api/json/v1/1/random.php"
 
-  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+  fetch(url)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
     console.log(data)
-    
+    document.querySelector('#meal-name').innerText = data.meals[0].strMeal;
+    document.querySelector('#meal-category').innerText = data.meals[0].strCategory;
+    document.querySelector('img').src = data.meals[0].strMealThumb;
   })
   .catch(err => {
       console.log(`error ${err}`)
@@ -13,12 +15,12 @@
 
   // const url = "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast"
 
-  fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-    console.log(data)
+  // fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+  // .then(res => res.json()) // parse response as JSON
+  // .then(data => {
+  //   console.log(data)
     
-  })
-  .catch(err => {
-      console.log(`error ${err}`)
-  });
+  // })
+  // .catch(err => {
+  //     console.log(`error ${err}`)
+  // });
