a4da8d98607b7a8635c99be2a5d446cea8bf90f4
klynner33 <113389068+klynner33@users.noreply.github.com>
2023-11-30 10:08:21 -0600

    Put measurements and ingredients from the api into the dom, resized the image, centered the get meal button

--- index.js
+++ index.js
@@ -8,10 +8,32 @@
     document.querySelector('#meal-name').innerText = data.meals[0].strMeal;
     document.querySelector('#meal-category').innerText = data.meals[0].strCategory;
     document.querySelector('img').src = data.meals[0].strMealThumb;
+    document.querySelector('a').href = data.meals[0].strSource;
+    console.log(Object.values(data.meals[0]));
+    let mealsArr = Object.values(data.meals[0])
+    for(let i = 29; i <= 48; i++) {
+      const li = document.createElement('li');
+      li.textContent = mealsArr[i];
+      document.querySelector('#measurements').appendChild(li);
+    }
+    for(let i = 9; i <= 28; i++) {
+      const li = document.createElement('li');
+      li.textContent = mealsArr[i];
+      document.querySelector('#ingredients').appendChild(li);
+    }
+
+
   })
   .catch(err => {
       console.log(`error ${err}`)
   });
+
+  // data.subclasses.forEach(obj => {
+  //   console.log(obj.name)
+  //   const li = document.createElement('li');
+  //   li.textContent = obj.name;
+  //   document.querySelector('ul').appendChild(li);
+  // })
 
   // const url = "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast"
 
